5ded612e5080562f1ed9544ae43ce2af55b97ee8
nabiladigo <nabiladigo91@gmail.com>
2022-03-22 21:27:11 -0400

    create app.js and components pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
 
-function App() {
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/contact";
+
+
+const App = () =>{
+  const URL='https://digo-portfolio.herokuapp.com/';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Nav />
+        <Routes>
+             <Route exact path='/' element ={<Home  URL={URL} />} />
+            <Route path='/projects' element = {<Projects  URL={URL} />} />
+            <Route path='/about' element = {<About  URL={URL} /> } />
+            <Route path='/contact' element= {<Contact URL={URL} />} />   
+        </Routes>
+         <Footer />
     </div>
-  );
+  )
 }
-
 export default App;
