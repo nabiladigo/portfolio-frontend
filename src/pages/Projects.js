b685fcd12550ad8ba59780d10ab93140786e0af3
nabiladigo <nabiladigo91@gmail.com>
2022-03-25 12:43:55 -0400

    adding projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState, useEffect  } from 'react';
 
-const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
+
+const Projects = (props) => {
+  const [projects, setProjects] = useState(null);
+  const getProjectsData = async() =>{
+    const response = await fetch(props.URL + "peojects");
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    setProjects(data);
+  };
+  useEffect(()=> getProjectsData)
+  const loaded =() =>{
+    return projects.map((project)=>(
+    <div>
+        <h1>{project.name}</h1>
+        <img src={project.image} alt={project.name}/>
+        <a href={project.git}>
+          <button>Github</button>
+        </a>
+        <a href={project.live}>
+          <button>live site</button>
+        </a>
+      </div>
+    ));
+  };
+
+  return projects ? loaded() : <h1>Loading...</h1>;
 }
 
 export default Projects
