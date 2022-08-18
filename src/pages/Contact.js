9c2e15336eef9df6c16d4004c3993bbd5e15c772
nabiladigo <nabiladigo91@gmail.com>
2022-08-18 13:19:14 -0400

    put contact scss

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,5 @@
  import React from "react";
-
+import '../styles/Contact.scss';
 
 const Contact = (props) => {
     return(
@@ -7,9 +7,9 @@ const Contact = (props) => {
         <h3 className="text-5xl font-bold pd-4">
             Get in Touch
           </h3>
-          <div className="box w-1/2 mx-auto py-8 border-solid border-4 rounded-lg sm:w-10/12">
+          <div  className="box w-1/2 mx-auto py-8 border-solid border-4 rounded-lg sm:w-10/12">
             <p className="font-bold text-2xl pb-16 sm:pb-8"> I like to hear from you</p>
-            <a href="mailto:nabiladigo91@gmail.com" className="mail hover:shadow font-bold py-3 px-8 text-xl rounded text-center">Say Hello 👋</a>
+            <a href="mailto:nabiladigo91@gmail.com" className=" email hover:shadow font-bold py-3 px-8 text-xl rounded text-center">Get in Touch 👋</a>
           </div>
       </div>
     );
